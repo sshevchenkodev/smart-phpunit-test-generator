1ef1de63bd528edfad8a5fd25e28030a162cfc4a
Stanislav <stanislav.shevchenko.brd@gmail.com>
2020-09-30 20:33:56 +0300

    Base structure and dependency

---
+++ src/SmartPHPUnitTestGenerator.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartPHPUnitGenerator;
+
+class SmartPHPUnitTestGenerator
+{
+    public function generate(): void
+    {
+        echo self::class . PHP_EOL;
+    }
+}
